3db60fa11919fd9f44031ea3d9d9f18386f6807a
Angelique Abacajan <ma_utsey@yahoo.com>
2021-09-16 15:44:00 -0400

    Completed and Passed task 2

--- index.js
+++ index.js
@@ -78,7 +78,29 @@ class Person {
 */
 
 class Car {
-  
+  constructor(model, milesPerGallon) {
+    this.model = model; 
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }  
+
+  fill(gallons) {
+    this.tank += gallons;
+  }
+
+  drive(distance) {
+    const ranOutOfFuel = this.tank  * this.milesPerGallon;
+    if(distance < ranOutOfFuel) {
+      this.odometer += distance;
+      this.tank -= distance / this.milesPerGallon;
+
+    } else {
+      this.tank = 0;
+      this.odometer += ranOutOfFuel;
+      return `I ran out of fuel at ${this.odometer} miles!`;
+    }
+  }
 }
 
 /*
